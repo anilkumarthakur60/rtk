1942eeb8663516dff491ffc741172bd8fc03b8c6
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-04-29 22:40:06 +0545

    update

--- react-rtk-auth/src/main.tsx
+++ react-rtk-auth/src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { StyledEngineProvider } from "@mui/material";
-import { ThemeProvider } from "@emotion/react";
 import store from "./store/store.ts";
 import { theme } from "./theme/theme.ts";
+
+import { StyledEngineProvider } from "@mui/material/styles";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
