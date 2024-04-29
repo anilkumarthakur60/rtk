b2c9784fe97f853ada369a25ae628236c64c8b4e
anil kumar thakur <anilkumarthakur60@gmail.com>
2024-04-30 00:35:37 +0545

    update

--- react-rtk-auth/src/App.tsx
+++ react-rtk-auth/src/App.tsx
@@ -1,29 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FrontendLayout from "./layouts/FrontendLayout";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import FrontendLayout from "./layouts/front/FrontendLayout";
 import IndexPage from "./pages/IndexPage";
 import RegisterPage from "./pages/RegisterPage";
-import BackendLayout from "./layouts/BackendLayout";
+import BackendLayout from "./layouts/back/BackendLayout";
 import Dashboard from "./pages/Dashboard";
 import PostPage from "./pages/PostPage";
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/LoginPage";
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<FrontendLayout />}>
-          <Route index element={<IndexPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-        </Route>
-        <Route path="/dashboard" element={<BackendLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="posts" element={<PostPage />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<FrontendLayout/>}>
+                    <Route index element={<IndexPage/>}/>
+                    <Route path="login" element={<LoginPage/>}/>
+                    <Route path="register" element={<RegisterPage/>}/>
+                </Route>
+                <Route path="/dashboard" element={<BackendLayout/>}>
+                    <Route index element={<Dashboard/>}/>
+                    <Route path="posts" element={<PostPage/>}/>
+                </Route>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
---
+++ react-rtk-auth/src/layouts/back/SidebarMenu.tsx
@@ -0,0 +1,73 @@
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+type Props = {
+  open: boolean;
+};
+
+const SidebarMenu = (props: Props) => {
+  const { open } = props;
+  return (
+    <>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+              >
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default SidebarMenu;
